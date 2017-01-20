b9c6acccc5589bb6a749e1110d6f655ded900e18
Tung <Tung@DESKTOP-EKOJK9E>
2017-01-21 00:26:17 +0700

    0:25 21/1/2017

--- FMIP/MainWindow.cpp
+++ FMIP/MainWindow.cpp
@@ -78,6 +78,22 @@ MainWindow::MainWindow(const wxString& Title) : wxFrame(nullptr, wxID_ANY, Title
 		return;
 	}
 #endif
+	OSVERSIONINFOEX OSVerInfo;
+	typedef void (WINAPI* Ppt_RtlGetVersion) (OSVERSIONINFOEX*);
+	Ppt_RtlGetVersion pfnRtlGetVersion = nullptr;
+	HMODULE hMod = LoadLibrary(L"ntdll.dll");
+	if (hMod != nullptr)
+	{
+		pfnRtlGetVersion = (Ppt_RtlGetVersion)GetProcAddress(hMod, "RtlGetVersion");
+	}
+	if (pfnRtlGetVersion != nullptr)
+	{
+		pfnRtlGetVersion(&OSVerInfo);
+	}
+	if (OSVerInfo.dwMajorVersion == 10 || OSVerInfo.wProductType == VER_NT_WORKSTATION)
+	{
+		wxLogDebug(wxT("Windows 10"));
+	}
 	wxMenu* ptrMenuRefresh = new wxMenu;
 	wxMenu* ptrMenuAbout = new wxMenu;
 	wxMenuBar* ptrMenuBar = new wxMenuBar;
